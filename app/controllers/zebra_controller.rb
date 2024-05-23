3cdf723a112c092d2b737473d7faf355329d684a
dak200423 <166195026+dak200423@users.noreply.github.com>
2024-05-23 00:31:39 +0000

    completed rock

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,17 @@
+class ZebraController <ApplicationController
+  def horse
+    moves = ["rock", "paper", "scissors"]
+
+    @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+    render({ :template => "game_templates/play_rock" })
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get("/rock", { :controller => "zebra", :action => "horse" })
 
+  get ("/paper", { :controller => "lion", :action => "tiger" })
+
+ # get ("/scissors", { :controller => "", :action => "" })
+
+  # get ("/", { :controller => "", :action => "" })
 end
